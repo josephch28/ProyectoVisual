dbd6ba7ff6ff1ab3e2b594afd2a8dad209de76c1
andyyyia <andresparedesg.ap@gmail.com>
2025-10-30 18:32:59 -0500

    Creación de Paredes.java 12

--- src/Interfaces/Paredes.java
+++ src/Interfaces/Paredes.java
@@ -10,6 +10,6 @@ package Interfaces;
  */
 public class Paredes {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo, hola");
     }
 }
